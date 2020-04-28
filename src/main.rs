ae8ab7bd45ad5e43192d9f00c9cab4e2d76cd5f9
Dmitry Kalmykov <dmitrykalmykov@MacBook-Pro-Dmitry.local>
2020-04-28 14:33:34 +0300

    Initial commit for application build v.0.0.1

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Application version v.0.0.1 from 28.04.2020
+const APP_VERSION: &'static str = "0.0.1";
+
+fn main() {
+    println!("Hello, %username%! Application version v.{}.", APP_VERSION);
+    println!("Application started.");
+}
