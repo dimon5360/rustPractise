1d928e997d05153a73c056de0ac753a84744fa65
dimon5360 <dimon5360@gmail.com>
2020-06-09 22:33:37 +0300

    Spinning square is builded. Build v.0.0.3 from 09.06.2020.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use glutin_window::GlutinWindow as WindowSettings;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-// use piston_window::*;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
@@ -20,11 +19,33 @@ impl App {
         use graphics::*;
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(GREEN, gl);
+
+            let transform = c
+                .transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
+
+            rectangle(RED, square, transform, gl);
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) { 
+        // rotate 2 radians per second
+        self.rotation += 2.0 * args.dt;
     }
 }
 
-// Application version v.0.0.2 from 09.06.2020
-const APP_VERSION: &'static str = "0.0.2";
+// Application version v.0.0.3 from 09.06.2020
+const APP_VERSION: &'static str = "0.0.3";
 
 /***************************************
  * @brief Main method in application
@@ -32,21 +53,30 @@ const APP_VERSION: &'static str = "0.0.2";
 fn main() {
     
     use piston_window::*;
+
     println!("Hello, %username%! Application version v.{}.", APP_VERSION);
     println!("Application started.");
+    let opengl  = OpenGL::V3_2;
 
-    let mut window: PistonWindow = WindowSettings::new(
-        "piston: hello_world",
-        [640, 480]
-    )
-    .exit_on_esc(true).build().unwrap();
+    let mut window: PistonWindow = WindowSettings::new("Spinning square", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics, _device| {
-            clear([1.0; 4], graphics);
-            rectangle(  [1.0, 0.0, 0.0, 1.0],
-                        [0.0, 0.0, 100.0, 100.0], 
-                        context.transform, graphics);
-        });
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
     }
  }
