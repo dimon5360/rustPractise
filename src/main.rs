e4b40a59dd0d1acfe9973a0582066294ebae979e
dimon5360 <dimon5360@gmail.com>
2020-09-15 20:28:46 +0300

    new version v.0.0.5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 // Application version v.0.0.3 from 09.06.2020
-const APP_VERSION: &'static str = "0.0.4";
+const APP_VERSION: &'static str = "0.0.5";
 
 /***************************************
  * @brief Main method in application
